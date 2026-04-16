4c03db280932fcd5391ba6daa2e556fe85a8b6fd
Igor Muller <igormullersantos@hotmail.com>
2026-04-16 08:59:41 -0300

    Adiciona cabeçalhos para tratamento de erro de CORS

--- api/index.ts
+++ api/index.ts
@@ -105,6 +105,22 @@ const server = new ApolloServer({
 
 export default startServerAndCreateNextHandler(server, {
   async context(req, res) {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    );
+
+    if (req.method === "OPTIONS") {
+      res.end();
+      return { req, res };
+    }
+
     return { req, res };
   },
 });
