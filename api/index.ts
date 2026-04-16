80f266f1e10f3266a9bf2154797a24b240e415b2
Igor Muller <igormullersantos@hotmail.com>
2026-04-16 11:19:01 -0300

    Remove unificação realizada novamente

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import axios from "axios";
-
-// 1. Defina o Schema aqui dentro
-const typeDefs = `#graphql
-  type Reading { referencia: String texto: String }
-  type Liturgia { celebracao: String! cor: String! leitura: Reading salmo: Reading evangelho: Reading }
-  type Query { liturgiaDoDia: Liturgia }
-`;
-
-// 2. Defina os Resolvers aqui dentro
-const resolvers = {
-  Query: {
-    liturgiaDoDia: async () => {
-      try {
-        const response = await axios.get("https://liturgia.up.railway.app/v2/");
-        return {
-          celebracao: response.data.liturgia,
-          cor: response.data.cor,
-          leitura: response.data.leituras.primeiraLeitura[0],
-          salmo: response.data.leituras.salmo[0],
-          evangelho: response.data.leituras.evangelho[0],
-        };
-      } catch (e) { throw new Error("Erro na API externa"); }
-    },
-  },
-};
+import { typeDefs } from "../src/schema";
+import { resolvers } from "../src/resolvers";
 
 const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
 const apolloHandler = startServerAndCreateNextHandler(server);
