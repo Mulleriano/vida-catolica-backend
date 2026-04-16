1a1d7c30b665ae3bacd54d62752fc7d260d1e0e5
Igor Muller <igormullersantos@hotmail.com>
2026-04-16 11:33:05 -0300

    Corrige importação dos types e resolvers

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { typeDefs } from "./schema";
-import { resolvers } from "./resolvers";
+import { typeDefs } from "./schema.ts";
+import { resolvers } from "./resolvers.ts";
 
 const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
 const apolloHandler = startServerAndCreateNextHandler(server);
