147bf42524f02cd897afe21ef75a9d9aef5d2cc3
Igor Muller <igormullersantos@hotmail.com>
2026-04-16 11:59:00 -0300

    Renova lógica de origens permitidas

--- api/index.ts
+++ api/index.ts
@@ -14,12 +14,11 @@ export default async function handler(req: any, res: any) {
 
   const origin = req.headers.origin;
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // if (origin && allowedOrigins.includes(origin)) {
-  //   res.setHeader("Access-Control-Allow-Origin", origin);
-  // } else if (!origin) {
-  //   res.setHeader("Access-Control-Allow-Origin", "*");
-  // }
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  } else if (!origin) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
 
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
