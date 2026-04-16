dcc4314999f88a5c03bd7f1200038b1986c77e6c
Igor Muller <igormullersantos@hotmail.com>
2026-04-16 11:27:50 -0300

    Move o resolver e o schema para pasta api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from "./schema.ts";
-import { resolvers } from "./resolvers.ts";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "../api/schema.ts";
+import { resolvers } from "../api/resolvers.ts";
 
 const server = new ApolloServer({
   typeDefs,
